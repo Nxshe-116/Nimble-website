d1d06d88e2484e2e240b5732335ba40860964340
Nashe Chagumaira <mairanxshe4@gmail.com>
2023-04-13 14:16:49 +0200

    Initial Commit

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+    test : Date = new Date();
+    focus;
+    focus1;
+    focus2;
+    constructor(private _router: Router) { }
+
+    ngOnInit() {}
+
+    createAcc() {
+        this._router.navigate(['/landing']);
+    }
+}
